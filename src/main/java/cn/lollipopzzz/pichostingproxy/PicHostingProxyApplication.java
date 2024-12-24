43784adde1faa478f981bb8f7b375864e7794127
jihua_zhang <zhangjihua0327@outlook.com>
2024-12-24 21:19:25 +0800

    init

---
+++ src/main/java/cn/lollipopzzz/pichostingproxy/PicHostingProxyApplication.java
@@ -0,0 +1,13 @@
+package cn.lollipopzzz.pichostingproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicHostingProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicHostingProxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lollipopzzz/pichostingproxy/PicHostingProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lollipopzzz.pichostingproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicHostingProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
